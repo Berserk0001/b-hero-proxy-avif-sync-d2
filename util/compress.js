ef9518aeee6c2768adfac263ead9e11dbfd7177e
denYOUvo <59727582+denYOUvo@users.noreply.github.com>
2023-07-20 17:14:02 +0700

    Add files via upload
    revert changes

--- util/compress.js
+++ util/compress.js
@@ -1,19 +1,31 @@
 const sharp = require("sharp");
 
-function compress(input, avif, grayscale, quality, originSize) {
-	const format = avif ? "avif" : "jpeg";
-        let compressionQuality = quality * 0.5;
+function compress(input, webp, grayscale, quality, originSize, metadata) {
+	let format = webp ? "webp" : "jpeg";
+	let imgWidth = metadata.width;
+	let imgHeight = metadata.height;
+	let compressionQuality = quality;
 
-        quality = Math.ceil(compressionQuality);
-
+	//workaround for webp max res limit
+	if (imgWidth >= 16383 || imgHeight >= 16383) {
+		format = 'jpeg';
+		compressionQuality *= 2.5;
+	} else {
+		format = 'webp';
+		compressionQuality *= 0.5;
+	}
+	
+        quality = Math.ceil(compressionQuality)
+	
 	return sharp(input)
 		.grayscale(grayscale)
 		.toFormat(format, {
 			quality: quality,
-                        effort: 0
+			mozjpeg: true,
+			effort: 6
 		})
 		.toBuffer({resolveWithObject: true})
-		.then(({data: output,info}) => {	// this way we can also get the info about output image, like height, width
+		.then(({data: output, info}) => {	// this way we can also get the info about output image, like height, width
 		// .toBuffer()
 		// .then( output => {
 			return {
--- util/shouldCompress.js
+++ util/shouldCompress.js
@@ -1,12 +1,11 @@
-const MIN_COMPRESS_LENGTH = 2048; // ~2Kb
-const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 10; // ~20Kb
+const MIN_COMPRESS_LENGTH = 1024;
+const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 20;  // ~20KB
 
-function shouldCompress(originType, originSize, avif) {
-	if (!originType.startsWith("image")) return false;
-        if (originSize === 0) return false;
-	if (avif && originSize < MIN_COMPRESS_LENGTH) return false;
-	if (	// if png or gif image, and size is less than compression limit (also shouldn't be avif)
-		!avif &&
+function shouldCompress(originType, originSize, webp) {
+	if ( !originType.startsWith("image") || originSize === 0) return false;
+	if (webp && originSize < MIN_COMPRESS_LENGTH) return false;
+	if (	// if png or gif image, and size is less than compression limit (also shouldn't be webp)
+		!webp &&
 		(originType.endsWith("png") || originType.endsWith("gif")) &&
 		originSize < MIN_TRANSPARENT_COMPRESS_LENGTH
 	) {
